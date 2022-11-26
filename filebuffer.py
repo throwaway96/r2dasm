d3e8f1711815f0f9dbbf51180778b2f3b745aecd
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2022-11-26 00:17:20 -0500

    clean up FileBuffer
    Delete the stuff that's not being used for now.

--- filebuffer.py
+++ filebuffer.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Provides a Python list-like wrapper around a file."""
+"""Provides a wrapper around a file."""
 
 from io import BufferedReader, SEEK_END, SEEK_SET
-from collections.abc import ByteString
-from typing import overload
 
-class FileBuffer(ByteString):
+class FileBuffer:
     """Basically a file."""
     file: BufferedReader
     _len: int | None = None
@@ -39,36 +37,3 @@ class FileBuffer(ByteString):
             self.file.seek(offset, SEEK_END)
 
         return self.file.read(size)
-
-    @overload
-    def __getitem__(self, key: int) -> int: ...
-    @overload
-    def __getitem__(self, key: slice) -> bytes: ...
-    def __getitem__(self, key: int | slice) -> bytes | int:
-        if isinstance(key, int):
-            if abs(key) >= len(self):
-                raise IndexError('Tried to read past end of file')
-
-            return self.read(key, 1)[0]
-        elif isinstance(key, slice):
-            if key.step is not None:
-                raise TypeError('Slices with step not supported')
-
-            if abs(key.start) >= len(self) or abs(key.stop) > len(self):
-                raise IndexError('Tried to read past end of file')
-
-            start: int
-            if key.start is None:
-                start = 0
-            else:
-                start = key.start if key.start >= 0 else len(self) - key.start
-
-            stop: int
-            if key.stop is None:
-                stop = 0
-            else:
-                stop = key.stop if key.stop >= 0 else len(self) - key.stop
-
-            return self.read(start, stop - start)
-        else:
-            raise TypeError('Index must be int or slice')
